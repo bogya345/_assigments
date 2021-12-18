0ec8ef4075ef51b00e2f8b97fe70aaa13078ea6a
bvolkov <b.volkov@solarl.ru>
2021-12-18 14:38:57 +0300

    a6

--- Assignment6_.c
+++ Assignment6_.c
@@ -15,15 +15,15 @@ int main(int argc, char **argv)
 	if (rank == 2) MPI_Send(&rbuf, 1, MPI_FLOAT, 0, 5, MPI_COMM_WORLD);
 	if (rank == 0) {
 		MPI_Probe(MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &status);
-		if (status.MPI_SOURCE == 1) {
+		if (status.MPI_TAG == 5) {
 			MPI_Recv(&ibuf, 1, MPI_INT, 1, 5, MPI_COMM_WORLD, &status);
 			MPI_Recv(&rbuf, 1, MPI_FLOAT, 2, 5, MPI_COMM_WORLD, &status);
-			cout << "Process 0 recv " << ibuf << " from process 1, " << rbuf << "from process 2\n";
+			cout << "Process 0 recv " << ibuf << " from process 1, " << rbuf << " from process 2\n";
 		}
-		else if (status.MPI_SOURCE == 2) {
+		else if (status.MPI_TAG == 5) {
 			MPI_Recv(&rbuf, 1, MPI_FLOAT, 2, 5, MPI_COMM_WORLD, &status);
 			MPI_Recv(&ibuf, 1, MPI_INT, 1, 5, MPI_COMM_WORLD, &status);
-			cout << "Process 0 recv " << rbuf << " from process 2, " << ibuf << "from process 1\n";
+			cout << "Process 0 recv " << rbuf << " from process 2, " << ibuf << " from process 1\n";
 		}
 	}
 	MPI_Finalize();
@@ -39,17 +39,18 @@ condition
 
 Run:
 1)
-mpic++ ./Assignment6_.c -o A6_1.exe 
+mpic++ ./Assignment6.c -o A6_1.exe 
 
 2)
 mpic++ ./Assignment6_.c -o A6_2.exe 
 
 Output:
 1)
-PS > mpiexec ./A6_1
+PS > mpiexec ./A6_1 -n 7
 Process 0 recv 1 from process 1, 2from process 2
 
 2)
-
+PS > mpiexec ./A6_2 -n 7
+Process 0 recv 1 from process 1, 2from process 2
 
 */
