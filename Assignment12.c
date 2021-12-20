b6300aaad70364cde69f97aede607a85bace85d1
bvolkov <b.volkov@solarl.ru>
2021-12-20 18:57:58 +0300

    a12

--- Assignment12.c
+++ Assignment12.c
@@ -9,14 +9,15 @@ MPI_Recv_init(rbuf[0], 1, MPI_FLOAT, prev, 5, MPI_COMM_WORLD, reqs[0]);
 MPI_Recv_init(rbuf[1], 1, MPI_FLOAT, next, 6, MPI_COMM_WORLD, reqs[1]);
 MPI_Send_init(sbuf[0], 1, MPI_FLOAT, prev, 6, MPI_COMM_WORLD, reqs[2]);
 MPI_Send_init(sbuf[1], 1, MPI_FLOAT, next, 5, MPI_COMM_WORLD, reqs[3]);
+
+MPI_Startall(4, reqs);
+
 for (i = ...) {
 	sbuf[0] = ...;
 	sbuf[1] = ...;
-	MPI_Startall(4, reqs);
-//your code
-	MPI_Waitall(4, reqs, stats);
-//your code
 }
+MPI_Waitall(4, reqs, stats);
+
 MPI_Request_free(reqs[0]);
 MPI_Request_free(reqs[1]);
 MPI_Request_free(reqs[2]);
