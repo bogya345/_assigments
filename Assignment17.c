2133a51a29f7c97a1861929d236ee2dabfdf8b33
bvolkov <b.volkov@solarl.ru>
2021-12-21 17:12:07 +0300

    a17

--- Assignment17.c
+++ Assignment17.c
@@ -16,10 +16,11 @@ int main(int argc, char **argv)
 		else b[i] = 'b';
 	}
 	
-	if (rank == 0) {
+	if (rank == 2) {
 		for (i = 0; i < 10; i++) {
-			coout << "a[" << i << "] == " << a[i] << "\tb[" << i << "] == " << b[i] << endl;
+			cout << "a[" << i << "] == " << a[i] << "\tb[" << i << "] == " << b[i] << endl;
 		}
+		cout << endl;
 	}
 	
 	position = 0;
@@ -35,11 +36,43 @@ int main(int argc, char **argv)
 		MPI_Unpack(buf, 100, &position, b, 10, MPI_CHAR, MPI_COMM_WORLD);
 	}
 	
-	if (rank == 0) {
+	if (rank == 2) {
 		for (i = 0; i < 10; i++) {
-			coout << "a[" << i << "] == " << a[i] << "\tb[" << i << "] == " << b[i] << endl;
+			cout << "a[" << i << "] == " << a[i] << "\tb[" << i << "] == " << b[i] << endl;
 		}
+		cout << endl;
 	}
 	
 	MPI_Finalize();
 }
+
+/*
+Assignment:
+
+Run:
+mpic++ Assignment17.cpp -o A17.exe
+
+Output:
+PS > mpiexec -n 8 A17
+a[0] == 3       b[0] == b
+a[1] == 3       b[1] == b
+a[2] == 3       b[2] == b
+a[3] == 3       b[3] == b
+a[4] == 3       b[4] == b
+a[5] == 3       b[5] == b
+a[6] == 3       b[6] == b
+a[7] == 3       b[7] == b
+a[8] == 3       b[8] == b
+a[9] == 3       b[9] == b
+
+a[0] == 1       b[0] == a
+a[1] == 1       b[1] == a
+a[2] == 1       b[2] == a
+a[3] == 1       b[3] == a
+a[4] == 1       b[4] == a
+a[5] == 1       b[5] == a
+a[6] == 1       b[6] == a
+a[7] == 1       b[7] == a
+a[8] == 1       b[8] == a
+a[9] == 1       b[9] == a
+*/
