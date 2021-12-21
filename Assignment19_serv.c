ff99c39e4892eff3ab7ec53ae36e4efa36e01352
Volkov Bogdan <32989968+bogya345@users.noreply.github.com>
2021-12-21 15:12:55 +0300

    a19

--- Assignment19_serv.c
+++ Assignment19_serv.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "mpi.h"
+#include <iostream>
+using namespace std;
 int main(int argc, char **argv)
 {
 	int r;
@@ -8,12 +10,26 @@ int main(int argc, char **argv)
 	MPI_Status status;
 	MPI_Comm intercomm;
 	MPI_Open_port(MPI_INFO_NULL, port_name);
-	printf("portname: %s\n", port_name);
+	
+// 	printf("portname: %s\n", port_name);
+	cout << "portname: " << port_name << endl;
+	
+	cout << "Waiting for the client ... " << endl;
 	MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
+	cout << "Client connected " << endl;
+	
+	cout << "Server sent value: " << r << endl;
+	
 	MPI_Recv(&r, 1, MPI_INT, 0, 0, intercomm, &status);
+	
+	cout << "Server got value: " << r << endl;
+	
 	MPI_Comm_free(&intercomm);
 	MPI_Close_port(port_name);
-	printf("Клиент отправил %d\n", r);
+	
+// 	printf("Клиент отправил %d\n", r);
+	cout << "Client sent " << r << endl;
+	
 	MPI_Finalize();
 	return 0;
 }
