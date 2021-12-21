e4ae70a67dacffc22491595402d0aa7692febd9e
Volkov Bogdan <32989968+bogya345@users.noreply.github.com>
2021-12-21 12:15:45 +0300

    a16

--- Assignment16.c
+++ Assignment16.c
@@ -6,9 +6,10 @@ int main(int argc, char **argv)
 	MPI_Comm comm_revs;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_split(MPI_COMM_WORLD, 1, size - rank, &comm_revs);
+	MPI_Comm_split(MPI_COMM_WORLD, rank%2, size - rank, &comm_revs);
 	MPI_Comm_rank(comm_revs, &rank1);
 	//Display rank and rank1
+	cout << "rank == " << rank << "\trank1 == " << rank1 << endl;
 	MPI_Comm_free(&comm_revs);
 	MPI_Finalize();
 }
