13a0fc67d0a092298cfcfc6a61e1042d41911e2c
bvolkov <b.volkov@solarl.ru>
2021-12-21 17:52:16 +0300

    a21

--- Assignment21.c
+++ Assignment21.c
@@ -5,15 +5,28 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	int rank;
+	const int bufSize = 100;
 	MPI_Init(&argc, &argv);
 	MPI_File fh;
-	char buf[10];
+	char buf[bufSize];
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_File_open(MPI_COMM_WORLD, "file1.txt", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-	MPI_File_set_view(fh, rank * 10, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
-	MPI_File_read_all(fh, buf, 10, MPI_CHAR, MPI_STATUS_IGNORE);
+	MPI_File_set_view(fh, rank * bufSize, MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+	MPI_File_read_all(fh, buf, bufSize, MPI_CHAR, MPI_STATUS_IGNORE);
 // 	printf("process %d, buf=%s\n", rank, buf);
 	cout << "process " << rank << ", buf=" << buf << endl; 
 	MPI_File_close(&fh);
 	MPI_Finalize();
 }
+
+/*
+Assignment:
+
+Run:
+mpic++ Assignment21.c -o A21
+
+Output:
+PS > mpiexec -n 1 A21
+process 0, buf=Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+
+*/
