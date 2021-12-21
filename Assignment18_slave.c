89c2de36539f1365e99cd6bd69652f08661090f8
Volkov Bogdan <32989968+bogya345@users.noreply.github.com>
2021-12-21 14:57:14 +0300

    a18

--- Assignment18_slave.c
+++ Assignment18_slave.c
@@ -1,4 +1,6 @@
 #include "mpi.h"
+#include <iostream>
+using namespace std;
 int main(int argc, char **argv)
 {
 	int rank;
@@ -7,6 +9,7 @@ int main(int argc, char **argv)
 	MPI_Comm_get_parent(&intercomm);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Send(&rank, 1, MPI_INT, 0, rank, intercomm);
+	cout << "Proc. #" << rank << " : Send message(" << rank << ")" << endl; 
 	MPI_Finalize();
 	return 0;
 }
