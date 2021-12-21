44d63e2e94d3af47b4e8e94ac1d847b2f2169137
bvolkov <b.volkov@solarl.ru>
2021-12-21 17:42:01 +0300

    a19

--- Assignment19_client.c
+++ Assignment19_client.c
@@ -5,24 +5,40 @@
 using namespace std;
 int main(int argc, char **argv)
 {
-	int rank, value;
+	int rank, value = 42;
 	MPI_Init(&argc, &argv);
 	char port_name[MPI_MAX_PORT_NAME];
 	MPI_Comm intercomm;
+	MPI_Status status;
 	strcpy(port_name, argv[1]);
 	
 	cout << "Attempt to connect " << endl;
 	MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
-	cout << "Server connection " << endl
+	cout << "Server connection " << endl;
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
 	cout << "Client sent value: " << value << endl;
-	MPI_Send(&rank, 1, MPI_INT, 0, 0, intercomm);
+	MPI_Send(&value, 1, MPI_INT, 0, 0, intercomm);
 	
-	MPI_Rec($value, 1, MPI_INT, 0, 0, intercomm);
+	// MPI_Recv(&value, MPI_INT, 0, intercomm);
+	MPI_Recv(&value, 1, MPI_INT, 0, 0, intercomm, &status);
 	cout << "Client got value: " << value << endl;
 	
 	MPI_Finalize();
 	return 0;
 }
+
+/*
+Assignment:
+
+Run:
+mpic++ Assignment19_serv.c -o serv.exe | mpic++ Assignment19_client.c -o client
+
+Output:
+PS > mpiexec -n 1 client 'ncalrpc:LAPTOP-J9VBK914[e2046380-7122-452f-8870-907d03e3ae5a] ncacn_ip_tcp:LAPTOP-J9VBK914[63470] 1'
+Attempt to connect
+Server connection
+Client sent value: 42
+Client got value: 25
+*/
--- Assignment19_serv.c
+++ Assignment19_serv.c
@@ -4,7 +4,7 @@
 using namespace std;
 int main(int argc, char **argv)
 {
-	int r;
+	int r = 25;
 	MPI_Init(&argc, &argv);
 	char port_name[MPI_MAX_PORT_NAME];
 	MPI_Status status;
@@ -23,13 +23,30 @@ int main(int argc, char **argv)
 	MPI_Recv(&r, 1, MPI_INT, 0, 0, intercomm, &status);
 	
 	cout << "Server got value: " << r << endl;
+	r = 25;
+	MPI_Send(&r, 1, MPI_INT, 0, 0, intercomm);
 	
 	MPI_Comm_free(&intercomm);
 	MPI_Close_port(port_name);
 	
 // 	printf("Клиент отправил %d\n", r);
-	cout << "Client sent " << r << endl;
+	// cout << "Client sent " << r << endl;
 	
 	MPI_Finalize();
 	return 0;
 }
+
+/*
+Assignment:
+
+Run:
+mpic++ Assignment19_serv.c -o serv.exe | mpic++ Assignment19_client.c -o client
+
+Output:
+PS > mpiexec -n 1 serv
+portname: ncalrpc:LAPTOP-J9VBK914[e2046380-7122-452f-8870-907d03e3ae5a] ncacn_ip_tcp:LAPTOP-J9VBK914[63470] 1
+Waiting for the client ...
+Client connected
+Server sent value: 25
+Server got value: 42
+*/
